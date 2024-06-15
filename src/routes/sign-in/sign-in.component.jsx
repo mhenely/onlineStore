91254cddff352f4907f7add4b7b3dc709f00b128
Matt Henely <matt.henely8@gmail.com>
2024-06-15 14:21:20 -0400

    finished setting up firebase userAuth

--- src/routes/sign-in/sign-in.component.jsx
+++ src/routes/sign-in/sign-in.component.jsx
@@ -1,9 +1,19 @@
+import { signInWithGoolePopup, createUserDocumentFromAuth } from '../../utils/firebase/firebase.utils';
+
 const SignIn = () => {
 
+  const logGoogleUser = async() => {
+    // getting user from firebase auth response 
+    const { user } = await signInWithGoolePopup();
+    console.log(user);
+    createUserDocumentFromAuth(user)
+  }
   return (
     <div>
       <h1>Sign In Here</h1>
+      <button onClick={logGoogleUser}>Click to Sign in With Google</button>
     </div>
+
   )
 }
 
--- src/utils/firebase/firebase.utils.js
+++ src/utils/firebase/firebase.utils.js
@@ -1,6 +1,7 @@
 import { initializeApp } from 'firebase/app';
 import { getAuth, signInWithRedirect, signInWithPopup, GoogleAuthProvider } from 'firebase/auth';
-
+import { set } from 'firebase/database';
+import { getFirestore, doc, getDoc, setDoc } from 'firebase/firestore';
 
 
 // Your web app's Firebase configuration
@@ -21,3 +22,38 @@ const provider = new GoogleAuthProvider();
 provider.setCustomParameters({
   prompt: "select_account"
 });
+
+export const auth = getAuth();
+export const signInWithGoolePopup = () => signInWithPopup(auth, provider);
+
+
+// creating firestore database connection
+export const db = getFirestore();
+
+export const createUserDocumentFromAuth = async (userAuth) => {
+  // userAuth 
+  const userDocRef = doc(db, 'users', userAuth.uid)
+
+  const userSnapshot = await getDoc(userDocRef);
+
+  // if user data does not exist
+  if (!userSnapshot.exists()) {
+    // create a user in the DB
+    const { displayName, email } = userAuth;
+    const createdAt = new Date();
+
+    try {
+      await setDoc(userDocRef, {
+        displayName,
+        email,
+        createdAt
+      })
+    }
+    catch (error) {
+      console.log('Error while creating the user', error.message);
+    }
+  }
+  // if user data does not exist
+  return userDocRef;
+
+}
